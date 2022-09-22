cf65370dbc9d6e98d36776e2d4e25c2abe765885
Sh0ckFR <10033649+Sh0ckFR@users.noreply.github.com>
2022-09-22 14:22:40 +0200

    Added DLLirant 0.5 (new version in .NET)

---
+++ DLLirant.NET/Classes/FileOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace DLLirant.NET.Classes
+{
+    internal class FileOperations
+    {
+        public void CreateDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
+
+        public void DeleteDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.Delete(path, true);
+                } catch (UnauthorizedAccessException) { }
+            }
+        }
+
+        public void CopyFile(string file)
+        {
+            if (!File.Exists($"output/{Path.GetFileName(file)}"))
+            {
+                File.Copy(file, $"output/{Path.GetFileName(file)}");
+            }
+        }
+
+        public void RenameFile(string path, string newpath)
+        {
+            if (File.Exists(path))
+            {
+                File.Move(path, newpath);
+            }
+        }
+    }
+}
